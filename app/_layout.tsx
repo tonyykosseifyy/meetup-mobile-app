6c4515c95468f204a2bffa69ab7f20edbd49189e
tonyykosseifyy <tonykosseify123@gmail.com>
2024-02-11 13:00:16 +0200

    add: cabaret color as a tailwind custom color

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,5 +1,4 @@
 import FontAwesome from '@expo/vector-icons/FontAwesome';
-// import { useFonts } from 'expo-font';
 import { Stack } from 'expo-router';
 import * as SplashScreen from 'expo-splash-screen';
 import { useEffect } from 'react';
--- tailwind.config.js
+++ tailwind.config.js
@@ -7,6 +7,21 @@ module.exports = {
   ],
   theme: {
     extend: {
+      colors: {
+        cabaret: {
+          50: '#fbf4f7',
+          100: '#f9eaf0',
+          200: '#f5d5e0',
+          300: '#eeb3c6',
+          400: '#e284a1',
+          500: '#d14d72',
+          600: '#c2405e',
+          700: '#a72f47',
+          800: '#8b293c',
+          900: '#742736',
+          950: '#46111b',
+      },      
+      },
       fontFamily: {
         sans: [
           'Inter_400Regular', 
