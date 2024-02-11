42d9988c796f885796d5f3bf4e8136d3a1f9cdc7
tonyykosseifyy <tonykosseify123@gmail.com>
2024-02-11 18:56:03 +0200

    add: full logo view component

--- components/logo/index.tsx
+++ components/logo/index.tsx
@@ -1,24 +1,27 @@
 import { Image, View } from "react-native";
 
-
 const FullLogo = () => {
-    return (
-        <Image className="" source={require("@/assets/images/full_logo.png")} alt="Logo Full" />
-    )
-}
+  return (
+    <View className="w-32 h-8 ">
+      <Image
+        className="w-full h-full object-contain"
+        source={require("@/assets/images/full_logo3.png")}
+        alt="Logo Full"
+      />
+    </View>
+  );
+};
 
 const Logo = () => {
-    return (
-        <Image src="@/assets/images/logo.png" alt="Logo" />
-    )
-}
+  return <Image src="@/assets/images/logo.png" alt="Logo" />;
+};
 
 const LogoNavbar = () => {
-    return (
-        <View className="flex flex-row items-center justify-center w-full">
-            <FullLogo />
-        </View>
-    )
-}
+  return (
+    <View className="flex flex-column items-center justify-center w-full">
+      <FullLogo />
+    </View>
+  );
+};
 
 export { FullLogo, Logo, LogoNavbar };
