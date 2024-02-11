1c2458d1a9c3b34dc472abe294aacfe55dcd1219
tonyykosseifyy <tonykosseify123@gmail.com>
2024-02-11 19:22:23 +0200

    add: image prelogin screen

--- app/index.tsx
+++ app/index.tsx
@@ -1,15 +1,19 @@
-import { Link } from 'expo-router';
-import { Text, View, Image } from 'react-native';
-import { LogoNavbar } from '@/components/logo';
-
+import { Link } from "expo-router";
+import { Text, View, Image } from "react-native";
+import { LogoNavbar } from "@/components/logo";
 
 export default function PreLogin() {
   return (
-      <View className="bg-white flex-1">
-        <LogoNavbar />
-        <View className='w-full'>
-          <Image source={require("@/assets/images/pre_login.png")} alt="Pre Login" className="w-full h-96" />
-        </View>
+    <View className="bg-white flex-1">
+      <LogoNavbar />
+      <View className="w-full h-96 mt-10">
+        <Image
+          source={require("@/assets/images/pre_login.png")}
+          alt="Pre Login"
+          className="w-full h-full"
+          resizeMode="contain"
+        />
       </View>
+    </View>
   );
-};
+}
