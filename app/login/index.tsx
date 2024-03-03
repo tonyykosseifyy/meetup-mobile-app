11e11a21e0edb18abf3a2cf20236198739d74628
tonyykosseifyy <tonykosseify123@gmail.com>
2024-03-04 01:21:03 +0200

    add: header component

--- app/login/index.tsx
+++ app/login/index.tsx
@@ -1,11 +1,10 @@
-import { View } from "react-native";
+import { View, TouchableOpacity, Text } from "react-native";
 import Header from "@/components/header";
 
 export default function Login() {
   return (
     <View className="flex-1 bg-white">
       <Header leftButton />
-      
     </View>
   );
 }
--- components/header/index.tsx
+++ components/header/index.tsx
@@ -1,28 +1,30 @@
-import { View } from "react-native";
+import { Pressable, TouchableOpacity, View, Text } from "react-native";
 import { LogoNavbar } from "@/components/logo";
-import { FontAwesome6 } from '@expo/vector-icons';
+import { FontAwesome6 } from "@expo/vector-icons";
 import { HeaderProps } from "./interface.header";
+import { router } from "expo-router";
+import { Button } from "../button";
 
 const Header = (props: HeaderProps) => {
   const { leftButton, rightButton } = props;
+  const onPress = () => {
+    console.log("backkk");
+    router.back();
+  };
 
   return (
     <View className="p-5 flex flex-row items-center justify-between">
-        <View className="flex-1">
-            {leftButton && (
-                <FontAwesome6 name="arrow-left" size={22} color="black" />
-            )}
-        </View>
-        <View className="flex-1">
-            <LogoNavbar />
-        </View>
-        <View className="flex-1">
-        {rightButton && (
-                rightButton
-            )}
-        </View>
+      <View className="flex-1 ">
+        <TouchableOpacity onPress={onPress}>
+          <FontAwesome6 name="arrow-left" size={22} color="black" />
+        </TouchableOpacity>
+      </View>
+      <View className="flex-1">
+        <LogoNavbar />
+      </View>
+      <View className="flex-1">{rightButton && rightButton}</View>
     </View>
-  )
+  );
 };
 
 export default Header;
