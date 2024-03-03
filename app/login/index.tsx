be38e822060deb94acf50f4e9a5e8b8cee9d7493
tonyykosseifyy <tonykosseify123@gmail.com>
2024-03-04 01:23:44 +0200

    designing login page

--- app/login/index.tsx
+++ app/login/index.tsx
@@ -5,6 +5,9 @@ export default function Login() {
   return (
     <View className="flex-1 bg-white">
       <Header leftButton />
+      <View>
+        <Text className="text-cabaret-500 text-center font-medium">Share Smiles, Women's Connections</Text>
+      </View>
     </View>
   );
 }
