9f267d277ad79abacca54f7d6846d572068ab6ce
tonyykosseifyy <tonykosseify123@gmail.com>
2024-03-04 12:35:36 +0200

    add: prettier for code fomatting - eslint for code quality rules

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,10 +1,7 @@
 /** @type {import('tailwindcss').Config} */
 
 module.exports = {
-  content: [
-    "./app/**/*.{js,jsx,ts,tsx}",
-    "./components/**/*.{js,jsx,ts,tsx}"
-  ],
+  content: ["./app/**/*.{js,jsx,ts,tsx}", "./components/**/*.{js,jsx,ts,tsx}"],
   theme: {
     extend: {
       colors: {
@@ -12,41 +9,41 @@ module.exports = {
           500: "#FEF2F4",
         },
         cabaret: {
-          50: '#fbf4f7',
-          100: '#f9eaf0',
-          200: '#f5d5e0',
-          300: '#eeb3c6',
-          400: '#e284a1',
-          500: '#d14d72',
-          600: '#c2405e',
-          700: '#a72f47',
-          800: '#8b293c',
-          900: '#742736',
-          950: '#46111b',
-      },      
+          50: "#fbf4f7",
+          100: "#f9eaf0",
+          200: "#f5d5e0",
+          300: "#eeb3c6",
+          400: "#e284a1",
+          500: "#d14d72",
+          600: "#c2405e",
+          700: "#a72f47",
+          800: "#8b293c",
+          900: "#742736",
+          950: "#46111b",
+        },
       },
       fontFamily: {
         sans: [
-          'Inter_400Regular', 
-          'Inter_500Medium', 
-          'Inter_600SemiBold',
-          'Inter_700Bold',
-          'system-ui', 
-          '-apple-system', 
-          'BlinkMacSystemFont', 
-          '"Segoe UI"', 
-          'Roboto', 
-          '"Helvetica Neue"', 
-          'Arial', 
-          '"Noto Sans"', 
-          'sans-serif', 
-          '"Apple Color Emoji"', 
-          '"Segoe UI Emoji"', 
-          '"Segoe UI Symbol"', 
-          '"Noto Color Emoji"'
+          "Inter_400Regular",
+          "Inter_500Medium",
+          "Inter_600SemiBold",
+          "Inter_700Bold",
+          "system-ui",
+          "-apple-system",
+          "BlinkMacSystemFont",
+          '"Segoe UI"',
+          "Roboto",
+          '"Helvetica Neue"',
+          "Arial",
+          '"Noto Sans"',
+          "sans-serif",
+          '"Apple Color Emoji"',
+          '"Segoe UI Emoji"',
+          '"Segoe UI Symbol"',
+          '"Noto Color Emoji"',
         ],
       },
     },
   },
   plugins: [],
-}
+};
