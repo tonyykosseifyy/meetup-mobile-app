0ae8c8b4bc64cd5e7447965f1a34757d3c7eee3e
tonyykosseifyy <tonykosseify123@gmail.com>
2024-03-05 09:57:14 +0200

    modify: header component to accept left and right buttons

--- components/header/index.tsx
+++ components/header/index.tsx
@@ -8,22 +8,21 @@ import { Button } from "../button";
 const Header = (props: HeaderProps) => {
   const { leftButton, rightButton } = props;
   const onPress = () => {
-    console.log("backkk");
     router.back();
-    console.log("asd");
   };
 
   return (
     <View className="p-5 flex flex-row-reverse items-center justify-between">
-      <View className="flex-1"></View>
+      <View className="flex-1">{rightButton && rightButton}</View>
       <View className="flex-1">
         <LogoNavbar />
       </View>
       <View className="flex-1">
-        {rightButton && rightButton}
-        <TouchableOpacity onPress={onPress}>
-          <FontAwesome6 name="arrow-left" size={22} color="black" />
-        </TouchableOpacity>
+        {leftButton && (
+          <TouchableOpacity onPress={onPress}>
+            <FontAwesome6 name="arrow-left" size={22} color="black" />
+          </TouchableOpacity>
+        )}
       </View>
     </View>
   );
