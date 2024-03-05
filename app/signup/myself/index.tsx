1a594af5a78def0aaaa44b2b952dc80713649bc6
tonyykosseifyy <tonykosseify123@gmail.com>
2024-03-05 11:10:56 +0200

    add: header text for signup/myself screen

--- app/signup/myself/index.tsx
+++ app/signup/myself/index.tsx
@@ -1,11 +1,17 @@
 import React from "react";
-import { Text, View } from "react-native";
+import { ScrollView, Text, View } from "react-native";
 import { Header } from "@/components";
 
 export default function SignupMyself() {
   return (
     <View className="flex-1 bg-white flex">
       <Header leftButton />
+      <ScrollView className="mt-7 pl-5">
+        <View>
+          <Text className="font-medium text-2xl">Start your Story</Text>
+          <Text className="mt-1 opacity-70">Join Le2ine, find friends, and share laughs</Text>
+        </View>
+      </ScrollView>
     </View>
   );
 }
