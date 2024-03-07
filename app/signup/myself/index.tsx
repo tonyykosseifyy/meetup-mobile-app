a72afa5c33f0a7be93562e2c4f9c7770cc66b788
tonyykosseifyy <tonykosseify123@gmail.com>
2024-03-07 16:07:41 +0200

    add: name occupation and email inputs to register screen

--- app/signup/myself/index.tsx
+++ app/signup/myself/index.tsx
@@ -1,9 +1,9 @@
 import React, { useState } from "react";
 import { ScrollView, Text, View, TextInput, StyleSheet } from "react-native";
 import { Header, Input } from "@/components";
+import { Ionicons, AntDesign, Feather, Fontisto } from "@expo/vector-icons";
 
 export default function SignupMyself() {
-
   return (
     <View className="flex-1 bg-white flex">
       <Header leftButton theme="light" />
@@ -13,15 +13,36 @@ export default function SignupMyself() {
           <Text className="mt-1 opacity-70">Join Le2ine, find friends, and share laughs</Text>
         </View>
         <View className="mt-10">
-          <Text>Name</Text>
-          <Input 
-            placeholder="Your Name"
-          />
-          <Input 
-            className="mt-4"
-            placeholder="Bio"
-            multiline
-          />
+          {/* name */}
+          <View className="py-2 px-5 bg-white h-14 rounded-lg flex flex-row items-center justify-between border-[1px] border-solid border-cabaret-500">
+            <Ionicons name="person-outline" size={19} color="black" style={{ opacity: 0.5 }} />
+            <TextInput
+              placeholder="Your Name"
+              className="flex-1 h-6 ml-3"
+              placeholderTextColor={"#666666"}
+            />
+          </View>
+          {/* occupation */}
+
+          <View className="mt-7 py-2 px-5 bg-white h-14 rounded-lg flex flex-row items-center justify-between border-[1px] border-solid border-cabaret-500">
+            <AntDesign name="profile" size={19} color="black" style={{ opacity: 0.5 }} />
+            <TextInput
+              placeholder="Your Occupation"
+              className="flex-1 h-6 ml-3"
+              placeholderTextColor={"#666666"}
+            />
+          </View>
+
+          {/* email */}
+          <View className="mt-7 py-2 px-5 bg-white h-14 rounded-lg flex flex-row items-center justify-between border-[1px] border-solid border-cabaret-500">
+            <Fontisto name="email" size={19} color="black" style={{ opacity: 0.5 }} />
+            <TextInput
+              placeholder="Your Email"
+              keyboardType="email-address"
+              className="flex-1 h-6 ml-3"
+              placeholderTextColor={"#666666"}
+            />
+          </View>
           {/* <TextInput
             multiline 
             className="" style={{ textAlignVertical: "top", height: 100 }} placeholder="name" 
@@ -32,14 +53,13 @@ export default function SignupMyself() {
   );
 }
 
-
 const styles = StyleSheet.create({
   input: {
     height: 100,
-    width: '80%',
+    width: "80%",
     margin: 12,
     borderWidth: 1,
-    alignSelf: 'flex-start',
+    alignSelf: "flex-start",
     padding: 10,
-  }
+  },
 });
