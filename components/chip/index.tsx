df7e48f85752afc2ceb0682649ab418314f4ffbb
tonyykosseifyy <tonykosseify123@gmail.com>
2024-03-17 16:54:28 +0200

    modify chips font size

--- components/chip/index.tsx
+++ components/chip/index.tsx
@@ -8,10 +8,10 @@ const Chip = ({ active, icon, text, onPress }: ChipProps) => {
     <Pressable
       style={styles.grey_shadow}
       onPress={onPress}
-      className={`flex flex-row items-center justify-center rounded-full py-3 px-6 mx-1 mt-3 bg-${active ? 'cabaret-500' : 'white'}`}
+      className={`flex flex-row items-center justify-center rounded-full py-3 px-6 mx-1 mt-3 bg-${active ? "cabaret-500" : "white"}`}
     >
       {icon}
-      <Text className={`text-${active ? 'white' : 'black'}`} >{text}</Text>
+      <Text className={`text-${active ? "white" : "black"} text-xs`}>{text}</Text>
     </Pressable>
   );
 };
