51940b834a6999042d20b6829abe2a308cb72e5a
tonyykosseifyy <tonykosseify123@gmail.com>
2024-03-17 23:15:30 +0200

    finish: settings page

--- app/(tabs)/settings/index.tsx
+++ app/(tabs)/settings/index.tsx
@@ -1,19 +1,15 @@
 import { Button } from "@/components";
 import React, { useState } from "react";
-import { View, Text, Image, ScrollView, Touchable, TouchableOpacity, Switch } from "react-native";
-import {
-  FontAwesome6,
-  FontAwesome,
-  EvilIcons,
-  Ionicons,
-  Octicons,
-  MaterialCommunityIcons,
-} from "@expo/vector-icons";
+import { View, Text, Image, ScrollView, TouchableOpacity, Switch } from "react-native";
+import { MaterialCommunityIcons } from "@expo/vector-icons";
 import { Link as ExpoLink } from "expo-router";
 import NotificationsIcon from "@/assets/icons/settings/notifications.svg";
 import ArrowIcon from "@/assets/icons/settings/arrow.svg";
 import LocationIcon from "@/assets/icons/settings/location.svg";
 import AccountDetailsIcon from "@/assets/icons/settings/account-details.svg";
+import LogoutIcon from "@/assets/icons/settings/logout.svg";
+import PrivacyIcon from "@/assets/icons/settings/privacy.svg";
+import AboutUsIcon from "@/assets/icons/settings/aboutus.svg";
 
 export default function Settings() {
   const [isEnabled, setIsEnabled] = useState(false);
@@ -69,72 +65,42 @@ export default function Settings() {
           </View>
         </TouchableOpacity>
 
-        <Button
-          addClassName="bg-[#F2F2F2] flex flex-row place-center mb-4"
-          textColor="black"
-          disableShadow
-          view
-        >
-          <View className="w-full flex flex-row justify-between place-center">
-            <View className="flex-row">
-              <View className="mr-4 ">
-                <Ionicons name="exit-outline" size={24} color="gray" />
-              </View>
-              <View className="">
-                <Text>Logout</Text>
-              </View>
-            </View>
-
-            <View className="">
-              <Text>{">"}</Text>
-            </View>
+        <TouchableOpacity className="mb-4 flex p-4 flex-row items-center justify-between bg-[#F2F2F2] rounded-lg">
+          <View className="flex flex-row items-center">
+            <LogoutIcon width={19} />
+            <Text className="ml-3">Logout</Text>
           </View>
-        </Button>
-        <View className="h-px bg-gray-300 my-2" />
-        <Text className="text-lg mb-4">Account Information</Text>
-        <Button
-          addClassName="bg-[#F2F2F2] flex flex-row place-center mb-4"
-          textColor="black"
-          disableShadow
-          view
-        >
-          <View className="w-full flex flex-row justify-between place-center">
-            <View className="flex-row">
-              <View className="mr-4 ">
-                <Octicons name="shield-check" size={24} color="gray" />
-              </View>
-              <View className="">
-                <Text>Privacy Policy</Text>
-              </View>
-            </View>
-
-            <View className="">
-              <Text>{">"}</Text>
-            </View>
+          <View>
+            <ArrowIcon width={19} />
           </View>
-        </Button>
-        <Button
-          addClassName="bg-[#F2F2F2] flex flex-row place-center mb-4"
-          textColor="black"
-          disableShadow
-          view
-        >
-          <View className="w-full bg flex flex-row justify-between place-center">
-            <View className="flex-row">
-              <View className="mr-4 ">
-                <MaterialCommunityIcons name="information-variant" size={24} color="gray" />
-              </View>
-              <View className="">
-                <Text>Logout</Text>
-              </View>
-            </View>
+        </TouchableOpacity>
 
-            <View className="">
-              <Text>{">"}</Text>
-            </View>
+        <View className="h-px bg-gray-300 my-6" />
+
+        <Text className="text-lg mb-4 mt-2">Other</Text>
+
+        <TouchableOpacity className="mb-4 flex p-4 flex-row items-center justify-between bg-[#F2F2F2] rounded-lg">
+          <View className="flex flex-row items-center">
+            <PrivacyIcon width={19} />
+            <Text className="ml-3">Privacy Policy</Text>
           </View>
-        </Button>
-        <View className="h-px bg-gray-300 my-2" />
+          <View>
+            <ArrowIcon width={19} />
+          </View>
+        </TouchableOpacity>
+
+        <TouchableOpacity className="mb-4 flex p-4 flex-row items-center justify-between bg-[#F2F2F2] rounded-lg">
+          <View className="flex flex-row items-center">
+            <AboutUsIcon width={19} />
+            <Text className="ml-3">About Us</Text>
+          </View>
+          <View>
+            <ArrowIcon width={19} />
+          </View>
+        </TouchableOpacity>
+
+      
+        <View className="h-12" />
       </View>
     </ScrollView>
   );
