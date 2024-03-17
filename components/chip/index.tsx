c8cfd0a0d812c3b7796ba1107ef9a13aa4e7870f
tonyykosseifyy <tonykosseify123@gmail.com>
2024-03-17 17:10:12 +0200

    create Icon component

--- components/chip/index.tsx
+++ components/chip/index.tsx
@@ -2,16 +2,17 @@ import React, { useEffect, useRef } from "react";
 import { Pressable, Text, Animated } from "react-native";
 import { ChipProps } from "./interface.chip";
 import styles from "@/constants/styles";
+import ReadingIcon from "@/assets/icons/interests/reading";
 
 const Chip = ({ active, icon, text, onPress }: ChipProps) => {
   return (
     <Pressable
       style={styles.grey_shadow}
       onPress={onPress}
-      className={`flex flex-row items-center justify-center rounded-full py-3 px-6 mx-1 mt-3 bg-${active ? "cabaret-500" : "white"}`}
+      className={`flex flex-row items-center justify-center rounded-full py-3 px-4 mx-1 mt-3 bg-${active ? "cabaret-500" : "white"}`}
     >
-      {icon}
-      <Text className={`text-${active ? "white" : "black"} text-xs`}>{text}</Text>
+      <ReadingIcon active={active} />
+      <Text className={`text-${active ? "white" : "black"} text-xs ml-2`}>{text}</Text>
     </Pressable>
   );
 };
