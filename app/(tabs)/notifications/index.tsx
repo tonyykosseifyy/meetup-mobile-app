2ec1cefa55a29aef98fc0b66d070fbb250dcd3f1
tonyykosseifyy <tonykosseify123@gmail.com>
2024-03-21 18:02:59 +0200

    set bottom sheet to closed on first render

--- app/(tabs)/notifications/index.tsx
+++ app/(tabs)/notifications/index.tsx
@@ -7,29 +7,19 @@ import { Image } from "react-native";
 import { useAuth } from "@/api/mutations/auth/AuthProvider";
 import { router } from "expo-router";
 import { getMe } from "@/api/axios/users";
+import { formatTimeTo12Hour } from "@/utils/common";
 
-function formatTimeTo12Hour(datetimeStr: string): string {
-  const date = new Date(datetimeStr);
 
-  // Extract hours and minutes
-  let hours = date.getUTCHours(); // Use getUTCHours() to get the time in UTC
-  const minutes = date.getUTCMinutes(); // Use getUTCMinutes() for minutes in UTC
-
-  // Determine AM or PM suffix
-  const ampm = hours >= 12 ? "PM" : "AM";
-
-  // Convert 24-hour time to 12-hour format
-  hours = hours % 12;
-  hours = hours ? hours : 12; // The hour '0' should be '12'
-
-  // Format minutes to be always two digits
-  const formattedMinutes = minutes.toString().padStart(2, "0");
-
-  // Construct formatted time string
-  const formattedTime = `${hours}:${formattedMinutes} ${ampm}`;
-
-  return formattedTime;
-}
+// interface MeetupRequestResponse {
+//   id: number;
+//   request_from: IUser;
+//   request_to: IUser;
+//   status: string;
+//   time_slots: any[];
+//   place_requests: any[];
+//   request_to_accepting: boolean;
+//   request_from_accepting: boolean;
+// }
 
 
 export default function Tab() {
@@ -39,6 +29,7 @@ export default function Tab() {
     retry: 2,
     queryFn: () => getMe(),
   });
+  console.log('data', data);
   if (isLoading) return <Text>Loading meetings...</Text>;
 
   return (
--- utils/common.ts
+++ utils/common.ts
@@ -33,4 +33,26 @@ const formatTime = (date: Date): string => {
   return `${hours}:${formattedMinutes} ${ampm}`;
 };
 
-export { formatDate, calculateAge, formatTime };
+function formatTimeTo12Hour(datetimeStr: string): string {
+  const date = new Date(datetimeStr);
+
+  // Extract hours and minutes
+  let hours = date.getUTCHours(); // Use getUTCHours() to get the time in UTC
+  const minutes = date.getUTCMinutes(); // Use getUTCMinutes() for minutes in UTC
+
+  // Determine AM or PM suffix
+  const ampm = hours >= 12 ? "PM" : "AM";
+
+  // Convert 24-hour time to 12-hour format
+  hours = hours % 12;
+  hours = hours ? hours : 12; // The hour '0' should be '12'
+
+  // Format minutes to be always two digits
+  const formattedMinutes = minutes.toString().padStart(2, "0");
+
+  // Construct formatted time string
+  const formattedTime = `${hours}:${formattedMinutes} ${ampm}`;
+
+  return formattedTime;
+}
+export { formatDate, calculateAge, formatTime, formatTimeTo12Hour };
