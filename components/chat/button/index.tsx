ed758a2778f72bb48977b5cfd7e0737df9f1baf8
tonyykosseifyy <tonykosseify123@gmail.com>
2024-03-21 11:42:04 +0200

    finish: styling the chat buttons

--- components/chat/button/index.tsx
+++ components/chat/button/index.tsx
@@ -10,10 +10,10 @@ interface ChatButtonProps {
 const ChatButton = ({ positive }: ChatButtonProps) => {
   return (
     <TouchableOpacity
-      className={`flex flex-row items-center py-2 pl-4 pr-5 ${positive ? "bg-cabaret-500 rounded-l-xl rounded-br-xl" : "bg-[#F5F3F3] rounded-r-xl rounded-bl-xl"}`}
+      className={`flex flex-row items-center py-2 pl-4 pr-6 ${positive ? "bg-cabaret-500 rounded-l-xl rounded-br-xl" : "bg-[#F5F3F3] rounded-r-xl rounded-bl-xl"}`}
     >
       {positive ? <CheckIcon /> : <CloseIcon />}
-      <Text className={`font-bold ${positive ? "text-white" : ""}`}>
+      <Text className={`font-bold ml-1 ${positive ? "text-white" : ""}`}>
         {positive ? "Agree" : "Decline"}
       </Text>
     </TouchableOpacity>
