676427a9399fca3801558559812a2f676fd3ea98
Tony Kosseify <tonykosseify123@gmail.com>
2024-03-26 22:49:53 +0200

    add: authApi file

--- api/services/auth.tsx
+++ api/services/auth.tsx
@@ -12,27 +12,21 @@ import {
   ISetUserRequest,
 } from "../../interfaces";
 
-const login = async ({ email, password }: ILoginRequest): Promise<ILoginResponse> => {
-  return await axios.post("/auth/login/", { email: email.toLowerCase(), password });
+const authApi = {
+  login: async ({ email, password }: ILoginRequest): Promise<ILoginResponse> =>
+    await axios.post("/auth/login/", { email: email.toLowerCase(), password }),
+  register: async ({ email, password }: IRegisterRequest): Promise<IRegisterResponse> =>
+    await axios.post("/auth/register/", { email: email.toLowerCase(), password }),
+  generateAccess: async ({ refreshToken }: IRefreshRequest): Promise<IRefreshResponse> =>
+    await axios.post("/auth/token/refresh/", { refresh: refreshToken }),
+  setUserInfo: async (userInfo: ISetUserRequest): Promise<ISetUserResponse> =>
+    await axios.post("/auth/userinfo/", userInfo),
+  verifyEmail: async ({
+    email,
+    password,
+    code,
+  }: IVerifyEmailRequest): Promise<IVerifyEmailResponse> =>
+    await axios.post("/auth/verify-email/", { email: email.toLowerCase(), password, code }),
 };
 
-const register = async ({ email, password }: IRegisterRequest): Promise<IRegisterResponse> => {
-  return await axios.post("/auth/register/", { email, password });
-};
-
-const generateAccess = async ({ refreshToken }: IRefreshRequest): Promise<IRefreshResponse> =>
-  await axios.post("/auth/token/refresh/", { refresh: refreshToken });
-
-const setUserInfo = async (userInfo: ISetUserRequest): Promise<ISetUserResponse> => {
-  return await axios.post("/auth/userinfo/", userInfo);
-};
-
-const verifyEmail = async ({
-  email,
-  password,
-  code,
-}: IVerifyEmailRequest): Promise<IVerifyEmailResponse> => {
-  return await axios.post("/auth/verify-email/", { email, password, code });
-};
-
-export { login, register, generateAccess, setUserInfo, verifyEmail };
+export default authApi;
