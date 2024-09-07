20c154e8d732041537aac8bd7009596eb6f8ffdb
Tony Kosseify <tonykosseify123@gmail.com>
2024-09-07 15:33:09 +0300

    fix: settings loading state

--- app/(tabs)/settings/index.tsx
+++ app/(tabs)/settings/index.tsx
@@ -56,124 +56,125 @@ export default function Settings() {
     retry: 1,
   });
   const queryClient = useQueryClient();
-  return (
-    <ScrollView className="flex-1 bg-white">
-      {isLoading ? (
+  if (isLoading) {
+    return (
+      <View className="flex-1 bg-white">
         <View className="flex-1 h-full bg-white flex items-center justify-center">
           <ActivityIndicator size="large" color="#d14d72" />
         </View>
-      ) : (
-        <>
-          <Image
-            source={require("@/assets/images/sample_avatar.jpeg")}
-            className="w-32 h-32 mt-8 mb-2 self-center rounded-full border-solid border-2 border-cabaret-500 mx-4"
-          />
-          <Text className="text-center font-bold text-lg">{userInfo?.full_name}</Text>
-          <View className="mx-4 mt-6">
-            <Text className="text-lg mb-4">General</Text>
+      </View>
+    );
+  }
+  return (
+    <ScrollView className="flex-1 bg-white">
+      <Image
+        source={require("@/assets/images/sample_avatar.jpeg")}
+        className="w-32 h-32 mt-8 mb-2 self-center rounded-full border-solid border-2 border-cabaret-500 mx-4"
+      />
+      <Text className="text-center font-bold text-lg">{userInfo?.full_name}</Text>
+      <View className="mx-4 mt-6">
+        <Text className="text-lg mb-4">General</Text>
 
-            <TouchableOpacity className="mb-4 flex p-4 h-16 flex-row items-center justify-between bg-[#F2F2F2] rounded-lg">
-              <View className="flex flex-row items-center">
-                <NotificationsIcon width={19} />
-                <Text className="ml-3">Notifications</Text>
-              </View>
-              <View>
-                <ArrowIcon width={19} />
-              </View>
-            </TouchableOpacity>
-
-            <View className="mb-4 flex p-4 h-16 flex-row items-center justify-between bg-[#F2F2F2] rounded-lg">
-              <View className="flex flex-row items-center">
-                <LocationIcon width={19} />
-                <Text className="ml-3">Location</Text>
-              </View>
-              <View>
-                <Switch
-                  trackColor={{ false: "#D9D9D9", true: "#D9D9D9" }}
-                  thumbColor={isEnabled ? "#D14D72" : "#f4f3f4"}
-                  ios_backgroundColor="#D9D9D9"
-                  onValueChange={toggleSwitch}
-                  value={isEnabled}
-                  style={{ transform: "scale(0.9)" }}
-                />
-              </View>
-            </View>
-
-            <View className="h-px bg-gray-300 my-6" />
-
-            <Text className="text-lg mb-4 mt-2">Account Information</Text>
-
-            <ExpoLink href="/(tabs)/settings/account-details" asChild>
-              <TouchableOpacity className="mb-4 flex p-4 h-16 flex-row items-center justify-between bg-[#F2F2F2] rounded-lg">
-                <View className="flex flex-row items-center">
-                  <AccountDetailsIcon width={19} />
-                  <Text className="ml-3">Account Details</Text>
-                </View>
-                <View>
-                  <ArrowIcon width={19} />
-                </View>
-              </TouchableOpacity>
-            </ExpoLink>
-
-            {/* Your interests */}
-            <ExpoLink href="/(tabs)/settings/interests" asChild>
-              <TouchableOpacity className="mb-4 flex p-4 h-16 flex-row items-center justify-between bg-[#F2F2F2] rounded-lg">
-                <View className="flex flex-row items-center">
-                  <MaterialIcons
-                    name="favorite-border"
-                    style={{ opacity: 0.6 }}
-                    size={20}
-                    color="#222222"
-                  />
-                  <Text className="ml-3">My Interests</Text>
-                </View>
-                <View>
-                  <ArrowIcon width={19} />
-                </View>
-              </TouchableOpacity>
-            </ExpoLink>
-
-            <TouchableOpacity
-              onPress={() => logoutPrompt(queryClient)}
-              className="mb-4 flex p-4 h-16 flex-row items-center justify-between bg-[#F2F2F2] rounded-lg"
-            >
-              <View className="flex flex-row items-center">
-                <LogoutIcon width={19} />
-                <Text className="ml-3">Logout</Text>
-              </View>
-              <View>
-                <ArrowIcon width={19} />
-              </View>
-            </TouchableOpacity>
-
-            <View className="h-px bg-gray-300 my-6" />
-
-            <Text className="text-lg mb-4 mt-2">Other</Text>
-
-            <TouchableOpacity className="mb-4 flex p-4 h-16 flex-row items-center justify-between bg-[#F2F2F2] rounded-lg">
-              <View className="flex flex-row items-center">
-                <PrivacyIcon width={19} />
-                <Text className="ml-3">Privacy Policy</Text>
-              </View>
-              <View>
-                <ArrowIcon width={19} />
-              </View>
-            </TouchableOpacity>
-
-            <TouchableOpacity className="mb-4 flex p-4 h-16 flex-row items-center justify-between bg-[#F2F2F2] rounded-lg">
-              <View className="flex flex-row items-center">
-                <AboutUsIcon width={19} />
-                <Text className="ml-3">About Us</Text>
-              </View>
-              <View>
-                <ArrowIcon width={19} />
-              </View>
-            </TouchableOpacity>
-
-            <View className="h-12" />
+        <TouchableOpacity className="mb-4 flex p-4 h-16 flex-row items-center justify-between bg-[#F2F2F2] rounded-lg">
+          <View className="flex flex-row items-center">
+            <NotificationsIcon width={19} />
+            <Text className="ml-3">Notifications</Text>
           </View>
-        </>
-      )}
+          <View>
+            <ArrowIcon width={19} />
+          </View>
+        </TouchableOpacity>
+
+        <View className="mb-4 flex p-4 h-16 flex-row items-center justify-between bg-[#F2F2F2] rounded-lg">
+          <View className="flex flex-row items-center">
+            <LocationIcon width={19} />
+            <Text className="ml-3">Location</Text>
+          </View>
+          <View>
+            <Switch
+              trackColor={{ false: "#D9D9D9", true: "#D9D9D9" }}
+              thumbColor={isEnabled ? "#D14D72" : "#f4f3f4"}
+              ios_backgroundColor="#D9D9D9"
+              onValueChange={toggleSwitch}
+              value={isEnabled}
+              style={{ transform: "scale(0.9)" }}
+            />
+          </View>
+        </View>
+
+        <View className="h-px bg-gray-300 my-6" />
+
+        <Text className="text-lg mb-4 mt-2">Account Information</Text>
+
+        <ExpoLink href="/(tabs)/settings/account-details" asChild>
+          <TouchableOpacity className="mb-4 flex p-4 h-16 flex-row items-center justify-between bg-[#F2F2F2] rounded-lg">
+            <View className="flex flex-row items-center">
+              <AccountDetailsIcon width={19} />
+              <Text className="ml-3">Account Details</Text>
+            </View>
+            <View>
+              <ArrowIcon width={19} />
+            </View>
+          </TouchableOpacity>
+        </ExpoLink>
+
+        {/* Your interests */}
+        <ExpoLink href="/(tabs)/settings/interests" asChild>
+          <TouchableOpacity className="mb-4 flex p-4 h-16 flex-row items-center justify-between bg-[#F2F2F2] rounded-lg">
+            <View className="flex flex-row items-center">
+              <MaterialIcons
+                name="favorite-border"
+                style={{ opacity: 0.6 }}
+                size={20}
+                color="#222222"
+              />
+              <Text className="ml-3">My Interests</Text>
+            </View>
+            <View>
+              <ArrowIcon width={19} />
+            </View>
+          </TouchableOpacity>
+        </ExpoLink>
+
+        <TouchableOpacity
+          onPress={() => logoutPrompt(queryClient)}
+          className="mb-4 flex p-4 h-16 flex-row items-center justify-between bg-[#F2F2F2] rounded-lg"
+        >
+          <View className="flex flex-row items-center">
+            <LogoutIcon width={19} />
+            <Text className="ml-3">Logout</Text>
+          </View>
+          <View>
+            <ArrowIcon width={19} />
+          </View>
+        </TouchableOpacity>
+
+        <View className="h-px bg-gray-300 my-6" />
+
+        <Text className="text-lg mb-4 mt-2">Other</Text>
+
+        <TouchableOpacity className="mb-4 flex p-4 h-16 flex-row items-center justify-between bg-[#F2F2F2] rounded-lg">
+          <View className="flex flex-row items-center">
+            <PrivacyIcon width={19} />
+            <Text className="ml-3">Privacy Policy</Text>
+          </View>
+          <View>
+            <ArrowIcon width={19} />
+          </View>
+        </TouchableOpacity>
+
+        <TouchableOpacity className="mb-4 flex p-4 h-16 flex-row items-center justify-between bg-[#F2F2F2] rounded-lg">
+          <View className="flex flex-row items-center">
+            <AboutUsIcon width={19} />
+            <Text className="ml-3">About Us</Text>
+          </View>
+          <View>
+            <ArrowIcon width={19} />
+          </View>
+        </TouchableOpacity>
+
+        <View className="h-12" />
+      </View>
     </ScrollView>
   );
 }
