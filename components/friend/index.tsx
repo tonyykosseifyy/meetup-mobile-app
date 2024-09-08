ab4669d03c317c4a33288a47b063339ecc1f167f
Tony Kosseify <tonykosseify123@gmail.com>
2024-09-08 21:22:18 +0300

    style: fix friend card styling

--- components/friend/index.tsx
+++ components/friend/index.tsx
@@ -42,7 +42,10 @@ export const Friend = ({ item }: CardProps) => {
   });
 
   return (
-    <View className="bg-white px-2 pt-6 pb-6 relative" style={override_styles.shadow}>
+    <View
+      className="bg-white my-2 mx-2 px-2 pt-6 pb-6 relative rounded-3xl"
+      style={override_styles.shadow}
+    >
       <TouchableOpacity
         onPress={() => sendRequest({ userId: item.id! })}
         className="absolute z-10 top-8 right-5 w-12 h-8 flex-row justify-end"
@@ -51,7 +54,7 @@ export const Friend = ({ item }: CardProps) => {
       </TouchableOpacity>
       <View className="flex flex-row items-center mb-2">
         <View className="flex flex-row items-center h-full ">
-          <View className="w-14 h-14 p-[0.5px] rounded-full border-solid border-2 border-cabaret-500 mx-4">
+          <View className="w-14 h-14 p-1 bg-cabaret-100 rounded-2xl border-solid border border-cabaret-500 mx-2">
             <Image
               source={require("@/assets/avatars/adjusted_avatar_1.png")}
               className="w-full h-full rounded-full object-contain"
@@ -69,16 +72,38 @@ export const Friend = ({ item }: CardProps) => {
           </View>
         </View>
       </View>
-      <Text className="text-slate-500 text-[12px] mx-4 mt-4">{item.user_info?.biography}</Text>
+      <View className="w-full flex flex-row justify-center mt-2 px-2">
+        <View className="w-[100%] h-[1.5px] bg-slate-100 rounded-full"></View>
+      </View>
+      {/* About me */}
+      <View className="relative w-fit max-w-fit flex flex-col mt-4">
+        <Text className="text-cabaret-600 font-medium text-sm ml-2 mr-4">About me:</Text>
+        <View className="absolute w-7 -bottom-0.5 h-[1.5px] bg-cabaret-500 rounded-full ml-2 mr-4 mt-1"></View>
+      </View>
 
-      <ScrollView horizontal={true} className="ml-4 w-full pt-3 pb-1">
+      <Text className="text-slate-500 text-[13px] leading-5 ml-2 mr-4 mt-3">
+        {item.user_info?.biography} Lorem ipsum dolor sit amet consectetur adipisicing elit. Sunt,
+        odio. Deleniti recusandae sequi laborum ratione, exercitationem quas, autem necessitatibus
+        dolor sint beatae mollitia incidunt iure, vel reprehenderit odit. Similique, itaque.
+      </Text>
+
+      <View className="w-full flex flex-row justify-center mt-5 px-2">
+        <View className="w-[100%] h-[1.5px] bg-slate-100 rounded-full"></View>
+      </View>
+
+      {/* My Interests */}
+      <View className="relative w-fit max-w-fit flex flex-col mt-1">
+        <Text className="text-cabaret-600 font-medium text-sm ml-2 mr-4 mt-4">Things I Enjoy:</Text>
+        <View className="absolute w-12 -bottom-0.5 h-[1.5px] bg-cabaret-500 rounded-full ml-2 mr-4 mt-1"></View>
+      </View>
+      <View className="w-full mt-2 flex flex-row flex-wrap">
         {/* map through the interests and display them as chips */ item.user_info?.interests.map(
           (interest, index) => {
             return (
               <Chip
                 key={index}
-                pressableClassName="py-0 px-0 mr-2"
-                textClassName="ml-1"
+                pressableClassName="p-1 px-3 bg-cabaret-100"
+                textClassName="ml-1 text-[10px] text-cabaret-800"
                 text={interest.name}
                 onPress={() => {}}
                 active={false}
@@ -87,7 +112,7 @@ export const Friend = ({ item }: CardProps) => {
             );
           }
         )}
-      </ScrollView>
+      </View>
     </View>
   );
 };
@@ -99,8 +124,8 @@ const override_styles = StyleSheet.create({
       width: 0,
       height: 0,
     },
-    shadowOpacity: 0.2,
-    shadowRadius: 1,
+    shadowOpacity: 0.1,
+    shadowRadius: 5,
     elevation: 3,
   },
 });
